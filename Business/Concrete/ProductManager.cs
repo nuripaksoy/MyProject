50efc5b4d165211e9a5f19b11bad8b80922e9fb1
nuripaksoy <nuri99p@gmail.com>
2022-07-25 13:03:59 +0300

    11.ders

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,9 +31,9 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 11)
             {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+              return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
 
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
